2503ad61355dca41bdd4102f65a2bdf1238ec43b
Antonio César Fernandes <antoniocesarfernandes@gmail.com>
2022-12-16 14:01:08 -0300

    Trabalhando com módulos

---
+++ calculadora/calc.js
@@ -0,0 +1,11 @@
+var SomaFunc = require ("./somar");
+console.log(SomaFunc(2,3))
+
+var SubFunc = require ("./subtracao");
+console.log(SubFunc(7,5));
+
+var MultFunc = require("./multiplicacao");
+console.log(MultFunc(7,5));
+
+var MultDiv = require("./dividir");
+console.log(MultDiv(10,2));
