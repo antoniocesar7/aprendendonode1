22f1899d49eb4712222007440dd0e25a0f3732c7
Antonio César Fernandes <antoniocesarfernandes@gmail.com>
2023-01-04 15:27:30 -0300

    Iniciando rotas com parâmetros parte 1

---
+++ aula-08/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req,res){
+    res.send("Hello World!");
+});
+
+app.get("/sobre",function(req,res){
+    res.send("Minha página sobre");
+});
+
+app.get("/blog",function(req,res){
+    res.send("Bem-vindo ao meu blog!");
+});
+
+app.get('/ola',function(req,res){
+    res.send("Ola");
+})
+
+
+app.listen(8081,function(){
+    console.log("Servidor rodando na  url http://localhost:8081");
+});
