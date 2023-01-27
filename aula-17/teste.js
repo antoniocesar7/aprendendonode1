b9b4255d72aa8b3e26c3a2da110d872975a2553c
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-01-27 14:53:11 -0300

    Como se conectar ao MySQL, aula 17

---
+++ aula-17/teste.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('testenode', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
+});
+sequelize.authenticate().then(function() {
+    console.log("Conectado com sucesso!");
+}).catch(function(erro) {
+    console.log("Falha ao se conectar:" + erro);
+});
