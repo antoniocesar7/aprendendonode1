28f52514a4cf28ba584be6e9fe321eb0718ab58b
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-01-31 15:58:43 -0300

    Aula 18 do curso de node js - Models no Sequelize

---
+++ aula-18-models-no-sequelize/teste.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('testenode', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+// Postagem.create({
+//     titulo: "Um titulo qualquer",
+//     conteudo: "fasdfsadjçlfjsadçflkjsdçfkljsdçfklsjdçfklsdjçflksdjfçsdlkfjççsdklfjçsdlk"
+// })
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.create({
+    nome: "Antonio",
+    idade: 25,
+    email: "antoniocesar@email.com"
+})
+
+//Usuario.sync({ force: true })
