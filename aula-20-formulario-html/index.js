be9845f3581f994f9a61c9e163a2841ca33cd7b7
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-02-03 14:53:19 -0300

    Aula-20 - Como enviar dados do formulário

---
+++ aula-20-formulario-html/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
+
+
+//Config
+//Template Engine
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
+//app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+//Conexão com o Banco de Dados Mysql
+const sequelize = new Sequelize('testenode', 'root', '', {
+    host: "localhost",
+    dialect: 'mysql'
+})
+
+//Rotas
+app.get('/cad', function(req, res) {
+    //res.send('ROTA DE CADASTRO DE POSTS');
+    res.render('formulario'); //não precisa colocar a extensão handlebars
+})
+
+app.listen(8081, function() {
+    console.log("Servidor rodando na  url http://localhost:8081");
+});
