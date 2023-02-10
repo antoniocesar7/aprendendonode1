2165683fcc14f2cf0c1b9570abf1031889b758f2
antoniocesar7 <antoniocesarfernandes@gmail.com>
2023-02-10 14:38:24 -0300

    Pegando dados do formulário e inserindo no banco de dados

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize');
+const Post = require('./models/Post');
+// const Sequelize = require('sequelize');
 
 
 //Config
@@ -15,13 +16,20 @@ app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-//Conexão com o Banco de Dados Mysql
-const sequelize = new Sequelize('testenode', 'root', '', {
-    host: "localhost",
-    dialect: 'mysql'
-})
+// //Conexão com o Banco de Dados Mysql
+// const sequelize = new Sequelize('testenode', 'root', '', {
+//     host: "localhost",
+//     dialect: 'mysql'
+// })
 
 //Rotas
+
+//Quando cadastrar, passar para outra página... redirecionamento dentro Express
+//1º criar uma rota
+app.get('/',function(req, res){
+    res.render('home')
+})
+
 app.get('/cad', function(req, res) {
     //res.send('ROTA DE CADASTRO DE POSTS');
     res.render('formulario'); //não precisa colocar a extensão handlebars
@@ -30,7 +38,30 @@ app.get('/cad', function(req, res) {
 app.post('/add', function(req, res){
     //req.body.conteudo
     //res.send('FORMULÁRIO RECEBIDO!');
-    res.send("Texto: "+req.body.titulo+" Conteudo:"+req.body.conteudo)
+    //res.send("Texto: "+req.body.titulo+" Conteudo:"+req.body.conteudo)
+
+    // Post.create({
+    //     titulo: req.body.titulo,
+    //     conteudo: req.body.conteudo
+    // })
+    
+    // Post.create({
+    //     titulo: req.body.titulo,
+    //     conteudo: req.body.conteudo
+    // }).then(function(){
+    //     res.send("Post criado com sucesso!!!")
+    // }).catch(function(erro){
+    //     res.send("Houve um erro: "+ erro)
+    // })
+    
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(function(){
+        res.redirect('/')
+    }).catch(function(erro){
+        res.send("Houve um erro: "+ erro)
+    })
 })
 
 app.listen(8081, function() {
